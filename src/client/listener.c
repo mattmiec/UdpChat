d2579087089b3c4d50d26fac59d2802c14249470
matt <mtmiec@gmail.com>
2019-07-14 15:16:42 -0400

    cleanup client ack code flow and message output

--- src/client/listener.c
+++ src/client/listener.c
@@ -19,23 +19,42 @@ void *listen_handler(void* args)
     bool* acked = ((struct listen_handler_args*)args)->acked;
 
     struct packet inpacket;
-    struct sockaddr_in sockaddr;
-    socklen_t fromlen = sizeof(sockaddr);
+    struct sockaddr_in fromaddr;
+    socklen_t fromlen = sizeof(fromaddr);
     while (1)
     {
-        int bytes = recvfrom(socketfide, (void *)&inpacket, sizeof(inpacket), 0, (struct sockaddr*)&sockaddr, &fromlen);
+        int bytes = recvfrom(socketfide, (void *)&inpacket, sizeof(inpacket), 0, (struct sockaddr*)&fromaddr, &fromlen);
 
         if (strcmp(inpacket.type, "MESSAGE") == 0)
         {
-            char* buffer = malloc(strlen(inpacket.fromname) + strlen(": ") + strlen(inpacket.message));
-            strcpy(buffer, inpacket.fromname);
-            strcat(buffer, ": ");
-            strcat(buffer, inpacket.message);
-            send_ack(socketfide, inpacket.toname, inpacket.fromname);
-            puts(buffer);
-            printf(">>> ");
+            ///char* buffer = malloc(strlen(inpacket.fromname) + strlen(": ") + strlen(inpacket.message));
+            ///strcpy(buffer, inpacket.fromname);
+            ///strcat(buffer, ": ");
+            ///strcat(buffer, inpacket.message);
+
+            /// write output
+            ///puts(buffer);
+            printf("%s: %s\n>>> ", inpacket.fromname, inpacket.message);
             fflush(stdout);
-            free(buffer);
+
+            ///free(buffer);
+
+            send_ack(socketfide, inpacket.toname, inpacket.fromname);
+            /// construct and send ack packet
+            struct packet outpacket;
+            memset(&outpacket, 0, sizeof(outpacket));
+            strncpy(outpacket.type, "ACK", strlen("ACK"));
+            int bytes_sent = sendto(
+                    socketfide,
+                    &outpacket,
+                    sizeof(outpacket),
+                    0,
+                    (const struct sockaddr*)&fromaddr,
+                    sizeof(fromaddr)
+            );
+            if (bytes_sent < 0)
+                return 1;
+
         }
 
         if (strcmp(inpacket.type, "ACK") == 0)
@@ -45,7 +64,9 @@ void *listen_handler(void* args)
 
         if (strcmp(inpacket.type, "TABLE") == 0)
         {
+            /// delay here just to avoid out-of-order UI messages
             usleep(500000);
+            /// update user status table
             memcpy(user_table, inpacket.message, sizeof(user_table));
             printf("[Client table updated.]\n>>> ");
             fflush(stdout);
